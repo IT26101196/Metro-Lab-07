c9efddb873925fc5a93d3dc12ae3368cacea8fc1
IT26101196 <IT26101196@my.sliit.lk>
2026-09-14 21:31:20 +0530

    Add files via upload

---
+++ Q2/IT26101196Lab7Q2C.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class IT26101196Lab7Q2C {
+	
+	public static void main(String[] args){
+		
+		Scanner input = new Scanner(System.in);
+		
+        //For the rows
+		for (int j=5 ; j>=1 ; j--) {
+			
+			//For the column
+			for (int i=1 ; i<=j ; i++) {
+				
+				//Print number in row 
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+}
+				
+				
+			
+
+
+		
